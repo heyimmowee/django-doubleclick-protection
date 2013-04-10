70681b0cf05075c105f0a874671c2856440ca68f
Henning Kage <henning.kage@googlemail.com>
2013-04-10 15:26:18 +0200

    First initial functions

--- README.rst
+++ README.rst
@@ -3,6 +3,13 @@ django-doubleclick-protection
 
 A server-side attempt to prohibit double clicks in Django applications.
 
+The idea behind this was born with a Turbogears project we wrote in our 
+company. We wanted to create a solution to solve the double-click 
+problem without relying on a client-side solution. `Martin Brochhaus`__
+wrote the first version and I refactored it over the years, e.g. to
+use the filesystem instead of a database variant. This is an attempt to
+port the code to a reusable Django application.
+
 Installation
 ============
 
@@ -15,9 +22,23 @@ Installation
 #. Add ``django-doubleclick-protection`` to the list of installed apps::
 
 	INSTALLED_APPS = (
-		# ...,
-		'doubleclick_protection'
+		# ...
+		'doubleclick_protection',
+		)
+		
+#. Add the middleware right after Django's CSRF middleware::
+
+	MIDDLEWARE_CLASSES = (
+		# ...
+	    'django.middleware.csrf.CsrfViewMiddleware',
+		'doubleclick_protection.middleware.DoubleClickProtectionMiddleware',
+		# ...
 		)
 
 Settings
 ========
+
+``DCLICK_CACHE_DIR``
+  Directory for the tokens to be stored.
+  
+__ https://github.com/mbrochh
--- doubleclick_protection/middleware.py
+++ doubleclick_protection/middleware.py
@@ -2,7 +2,12 @@
 
 """Middleware class."""
 
+import os
+import time
+
+from django.conf import settings
 from django.middleware.csrf import get_token
+
 
 HTML_CONTENT_TYPES = (
     'text/html',
@@ -10,13 +15,78 @@ HTML_CONTENT_TYPES = (
 
 
 class DoubleClickProtectionMiddleware(object):
+    """Middleware to prevent multiple POST events with the same token."""
+    
+    def __init__(self):
+        self._cache_dir = settings.DCLICK_CACHE_DIR
+        self._create_directories()
+    
+    def _create_directories(self):
+        """Creates all necessary directories to store tokens."""
+        dirs = (self._cache_dir,
+                os.path.join(self._cache_dir, 'tokens'),
+                os.path.join(self._cache_dir, 'delivered_tokens'),
+                os.path.join(self._cache_dir, 'file_infos'),
+                os.path.join(self._cache_dir, 'received_tokens'))
+        for dir in dirs:
+            #cache_dir_lock.acquire()
+            try:
+                if not os.path.isdir(dir):
+                    try:
+                        os.makedirs(dir)
+                    except OSError, err:
+                        raise StandardError(
+                            'Could\'nt create directory %s: %s'
+                            % (dir, str(err)))
+            #finally:
+            #    cache_dir_lock.release()
+    
+    def add_token(self, token):
+        pass
+        
+    def get_token(self, request):
+        """Retrieves the current CSRF token from the request.
+        
+        :param request: :class:`HttpRequest`
+        """
+        return request.POST.get('csrfmiddlewaretoken', None)
+        
+    def token_exists(self, dir, token):
+        """Checks, whether the token exists as a file.
+
+        :param dir:
+        :param token:
+        """
+        try:
+            os.stat(os.path.join(self._cache_dir, dir, token))
+        except OSError:
+            return False
+        return True
+        
+    def remove_token(self, dir, token):
+        """Removes a token from a directory.
+        
+        :param dir:
+        :param token:
+        """
+        fname = os.path.join(self._cache_dir, dir, token)
+        try:
+            os.remove(fname)
+        except OSError, err:
+            raise StandardError('Could\'nt remove token %s in directory %s: %s'
+                % (token, dir, str(err)))
+                
+    def story_body(self):
+        """Saves the response into a file."""
 
     def process_request(self, request):
         # Check for request.user.is_anonymous() ?
         # Check for content-type?
         if request.method != 'POST':
             return
-        token = request.POST.get('csrfmiddlewaretoken', None)
+        token = self.get_token(request)
+        if token is None:
+            return
 
     #def process_response(self, request, response):
     #    pass
