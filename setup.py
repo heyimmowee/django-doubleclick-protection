7dab99e2a1ca8d30ac4e8e62553943af3a7df0a7
Henning Kage <henning.kage@googlemail.com>
2013-05-06 13:55:40 +0200

    Changed test dir structure and setup file

--- setup.py
+++ setup.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-packages = find_packages(exclude=('tests',))
-
-install_requires = [
-    'setuptools',
-    'Django']
-
-tests_require = []
-
-testing_extras = tests_require + [
-    'nose',
-    'coverage',
-    'virtualenv']
-
-docs_extras = [
-    'Sphinx',
-    'docutils']
+from setuptools import setup
+import re
+import os
+import sys
 
 
-setup(name='django-doubleclick-protection',
-      version='0.1',
-      description='Python Distribution Utilities',
-      author='Henning Kage',
-      author_email='henning.kage@gmail.com',
-      url='http://github.com/hkage/django-doubleclick-protection/',
-      packages=packages,
-      install_requires=install_requires,
-      extras_require={
-          'testing':testing_extras,
-          'docs':docs_extras,
-          },
-      tests_require=tests_require,
-      test_suite='doubleclick_protection.tests',
-     )
+def get_version(package):
+    """Return package version as listed in `__version__` in `init.py`."""
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """Return root package and all sub-packages."""
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """Return all files under the root package, that are not in a package themselves."""
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name='django-doubleclick-protection',
+    version=get_version(package),
+    url='http://github.com/hkage/django-doubleclick-protection',
+    license='',
+    description='',
+    author='Henning Kage',
+    author_email='henning.kage@gmail.com',
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=['Django>=1.3']
+)
